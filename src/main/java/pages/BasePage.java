c419f37326dc218695159dc5cf0159507f5f5849
AlexAksioma <alexmedqwerty@gmail.com>
2024-09-06 11:59:05 +0300

    created project structure

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,2 +1,12 @@
-package pages;public class BasePage {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    static WebDriver driver;
+
+    public static void setDriver(WebDriver wd){
+        driver = wd;
+    }
 }
